b6616fef7185743417d6002f0f5d5b6d9e0a2b2a
heber ochoa prieto <heber76@hotmail.com>
2022-04-19 15:47:21 -0700

    finish date Class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ConceptsPOO;
+
+Console.WriteLine("POO Concepts!");
+Console.WriteLine("===============");
+
+
+Date date1 = new Date(2022, 2, 11);
+
+try
+{
+    Console.WriteLine(new Date(2022, 2, 11));
+    Console.WriteLine(new Date(1976, 6, 1));
+    Console.WriteLine(new Date(1896, 12, 3));
+    Console.WriteLine(new Date(2024, 2, 29));
+
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+    
+}
+
